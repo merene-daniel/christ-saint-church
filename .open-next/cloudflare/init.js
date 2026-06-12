d7af9c23419a09c44ad060eadaa2368af9b16e66
Daniel Merene <daniel.merene@gmail.com>
2026-06-12 01:51:00 -0400

    Disable proxy for Cloudflare deployment

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1781242780986,
+    __BUILD_TIMESTAMP_MS__: 1781243397556,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
