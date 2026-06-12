44c7c94d463d5cfc2b2b954beca9de37fe8e67ea
Daniel Merene <daniel.merene@gmail.com>
2026-06-12 01:41:21 -0400

    Disable proxy for Cloudflare deployment

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"MONGODB_URI":"mongodb+srv://danielmerene:dm0105@cluster0.nks6i.mongodb.net/christ-saints-church?retryWrites=true&w=majority","NEXT_PUBLIC_SITE_NAME":"Christ Saints' Church","NEXT_PUBLIC_SITE_URL":"http://localhost:3000","ADMIN_PASSWORD":"admin1234"};
+export const development = {"MONGODB_URI":"mongodb+srv://danielmerene:dm0105@cluster0.nks6i.mongodb.net/christ-saints-church?retryWrites=true&w=majority","NEXT_PUBLIC_SITE_NAME":"Christ Saints' Church","NEXT_PUBLIC_SITE_URL":"http://localhost:3000","ADMIN_PASSWORD":"admin1234"};
+export const test = {};
